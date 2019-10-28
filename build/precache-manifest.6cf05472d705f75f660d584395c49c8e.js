10b60dcd461529b46c13a8f7331fba98d7c4308c
Parthiban Sudhaman <Parthi>
2019-10-28 21:48:39 +0530

    initial commit

---
+++ build/precache-manifest.6cf05472d705f75f660d584395c49c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a3b844dbae0ef3c7ef98a1222e68a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fca903d2d5b07e265e2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d27999996e8be8912c63",
+    "url": "/static/js/2.cd4553ac.chunk.js"
+  },
+  {
+    "revision": "9fca903d2d5b07e265e2",
+    "url": "/static/js/main.b3958e17.chunk.js"
+  },
+  {
+    "revision": "1c1791b632b3b045a106",
+    "url": "/static/js/runtime-main.43b2ccfc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
